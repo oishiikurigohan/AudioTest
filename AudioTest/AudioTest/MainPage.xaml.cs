333ab1b8d3960a02bc75ae3ed1ce1428cb6258d9
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2017-12-28 12:21:01 +0900

    Add project files.

---
+++ AudioTest/AudioTest/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CocosSharp;
+using Xamarin.Forms;
+
+namespace AudioTest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            this.CocosView.ViewCreated += (sender, e) =>
+            {
+                var gameView = sender as CCGameView;
+                var scene = new CCScene(gameView);
+                var layer = new CCLayerColor();
+                scene.AddLayer(layer);
+
+                var background = new CCSprite("background", null);
+                background.Position = layer.VisibleBoundsWorldspace.Center;
+                layer.AddChild(background);
+
+                var kurineko = new CCSprite("kurineko", null);
+                kurineko.Position = layer.VisibleBoundsWorldspace.Center;
+                layer.AddChild(kurineko);
+
+                gameView.RunWithScene(scene);
+            };
+        }
+
+        public void Meow(object sender, EventArgs e)
+        {
+            CCAudioEngine.SharedEngine.PlayEffect("meow");
+        }
+    }
+}
